695bd5187768b597ec768bacc6cb3a577605ac04
Luay Sakr <LuaySakr@gmail.com>
2019-02-24 03:14:52 +0200

    update

--- up.sh
+++ up.sh
@@ -1,1 +1,31 @@
-docker-compose up --build 
+
+
+
+
+# docker stop $(docker ps -a | grep -v "por" | awk 'NR>1 {print $1}')
+ rm -rf MicroServiceSimple-Customer 
+git clone https://github.com/LuaySakr/MicroServiceSimple-Customer.git
+
+
+rm -rf MicroServiceSimple-Dashboard 
+git clone https://github.com/LuaySakr/MicroServiceSimple-Dashboard.git
+
+
+rm -rf MicroServiceSimple-Pulsworker 
+git clone https://github.com/LuaySakr/MicroServiceSimple-Pulsworker.git
+
+
+ rm -rf MicroServiceSimple-Vehicle 
+git clone https://github.com/LuaySakr/MicroServiceSimple-Vehicle.git
+
+
+
+ rm -rf MicroServiceSimple-VehicleOwner 
+git clone https://github.com/LuaySakr/MicroServiceSimple-VehicleOwner.git
+
+
+
+
+
+
+docker-compose up --build  -d
